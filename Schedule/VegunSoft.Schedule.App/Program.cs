4cc19cb273d412378a97e4c47b903ed359a64b63
Nhan <nhan7285@hotmail.com>
2021-01-26 01:26:28 +0700

    .

--- Schedule/VegunSoft.Schedule.App/Program.cs
+++ Schedule/VegunSoft.Schedule.App/Program.cs
@@ -6,14 +6,26 @@ using VegunSoft.App.Service.Provider;
 using VegunSoft.Session.Service.Provider;
 using VegunSoft.Startup.Service;
 using VegunSoft.Startup.Service.Provider;
+using VegunSoft.Framework.Ioc.Apis;
+using VegunSoft.Framework.Ioc;
+using VegunSoft.Framework.Db;
+using VegunSoft.Session.Service.User;
+using VegunSoft.Session.Service.Model.Provider.User;
+using VegunSoft.Layer.EValue.App;
+using VegunSoft.Layer.Db.Meta.Enums;
 
 namespace VegunSoft.Schedule.App
 {
     static class Program
     {
+        private static IIocService _dbIoc;
+        static IIocService DbIoc => _dbIoc ?? (_dbIoc = XIoc.GetService(CDb.IocKey));
 
-        static IServiceStartup _serviceStartup;
+        private static IServiceStartup _serviceStartup;
         static IServiceStartup ServiceStartup => _serviceStartup ?? (_serviceStartup = new ServiceStartup());
+
+        private static IServiceLogin _serviceLogin;
+        static IServiceLogin ServiceLogin => _serviceLogin ?? (_serviceLogin = DbIoc.GetInstance<IServiceLogin>());
 
         /// <summary>
         /// The main entry point for the application.
@@ -22,14 +34,20 @@ namespace VegunSoft.Schedule.App
         static void Main()
         {
             ServiceStartup.Start().LoadModules(new List<Type>
-        {
-            typeof(Framework.Gui.Provider.WindowsForms.DevExp.XModule),
-            typeof(ModAppService),
+            {
+                typeof(Framework.Gui.Provider.WindowsForms.DevExp.XModule),
+                typeof(ModAppService),
            
-            typeof(ModSessionService),
+                typeof(ModSessionService),
            
-        });
-
+            });
+            ServiceLogin.SetState(new MLoginState()
+            {
+                BranchId = "DRV1",
+                DbGoal = EDbGoal.Real,
+                DbServer = EDbServer.Dev,
+                Username = "dev7285"
+            });
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
