ecad29d25ab23bd135e6f6cac4a024393ecb07a6
nhan7285 <nhan7285@gmail.com>
2021-02-03 04:22:44 +0700

    .

--- Customer/VegunSoft.Customer.View/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VegunSoft.Customer.View
-{
-    public class Class1
-    {
-    }
-}
---
+++ Customer/VegunSoft.Customer.View/Forms/IFBusTransfer.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using VegunSoft.Layer.Entity.Customer.Transfer;
+
+namespace VegunSoft.Layer.UcControl.Customer.Forms
+{
+    public interface IFBusTransfer
+    {
+        bool IsIn { get; set; }
+        bool IsOut { get; set; }
+        bool IsFinish { get; set; }
+        bool IsMultiBranch { get; set; }
+        void SelectTreatmentDepartment();
+
+        IFBusTransfer Init(object sender, IEntityCustomerStepMin fromStep, bool ChiHienThi_CLS = false);
+
+        DialogResult ShowDialog();
+
+        bool IsMoved { get; set; }
+
+        bool HasPrescriptions { get; set; }
+    }
+}
